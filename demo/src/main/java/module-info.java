1c55d3211666d852fb01e5330733a807e47f4175
Pavel Volkov <wolkow937@gmail.com>
2024-12-26 00:50:36 +0200

    changed module info

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -4,5 +4,7 @@ module com.example {
     requires java.sql;
 
     opens com.example to javafx.fxml;
+    opens com.example.entities to javafx.base;
+    exports com.example.entities;
     exports com.example;
 }
